e7680e81b8dcea02a5df5207e0ee3dcfd50b4dba
Gerald Chao <gerald@ml-illustrated.com>
2020-02-13 10:37:22 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-pytorch-basic',
+    packages=[],
+    version='0.0.1',
+    description='Basic Cookiecutter template for a Pytorch model',
+    author='Gerald',
+    license='BSD',
+    author_email='gerald@ml-illustratd.com',
+    url='https://github.com/ml-illustrated/cookiecutter-pytorch-basic',
+    keywords=['cookiecutter', 'template', 'pytorch'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,17 @@
+{% set is_open_source = cookiecutter.open_source_license != 'Not open source' -%}
+{% for _ in cookiecutter.project_name %}={% endfor %}
+{{ cookiecutter.project_name }}
+{% for _ in cookiecutter.project_name %}={% endfor %}
+
+{{ cookiecutter.project_short_description }}
+
+## Features
+
+- TODO
+
+## Credits
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
